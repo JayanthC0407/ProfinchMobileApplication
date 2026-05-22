8da4fdfd43887c8706857e3b13759effdb40ebf6
Jayanth Chintalapati <Jayanth.Chintalapati@PROFINCHBLR.COM>
2026-05-22 13:09:58 +0530

    created structure

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  AppColors._();
+  static const Color primary = Color.fromARGB(255, 224, 26, 55);
+  static const Color primaryDark = Color.fromARGB(255, 66, 0, 14);
+  static const Color light = Color.fromARGB(255, 236, 228, 230);
+}
